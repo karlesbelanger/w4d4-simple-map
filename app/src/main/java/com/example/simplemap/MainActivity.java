bcbb408d4de2bb5578a1a02260647919b9719081
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-27 00:01:29 -0400

    Add maps callbacks and map getter

--- app/src/main/java/com/example/simplemap/MainActivity.java
+++ app/src/main/java/com/example/simplemap/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.simplemap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "MainActivityTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.a_main_maps);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady: ");
     }
 }
